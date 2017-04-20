34be0f1bd448aa4ed5e7db275c9bd1325d8acc12
vlcik128 <vlckoadam1@gmail.com>
2017-04-20 20:43:19 +0200

    Update GamePhase.java

--- me/pataned/TIMV/enumerators/GamePhase.java
+++ me/pataned/TIMV/enumerators/GamePhase.java
@@ -1,6 +1,27 @@
 package me.pataned.TIMV.enumerators;
 
 public enum GamePhase {
-
+	
+	CEKANI(true), HLEDANI_CHEST(false), ENDERCHEST_TIME(false), HRA(false), RESET(false); 
+	
+	public static GamePhase currentPhase;
+	private boolean canJoin;
+	
+	GamePhase(boolean canJoinn){
+		this.canJoin = canJoinn;
+	}
+	
+	public boolean canJoin(){
+		return canJoin;
+	}
+	
+	public static GamePhase getPhase(){
+		return currentPhase;
+		
+	}
+	
+	public static void setPhase(GamePhase ph){
+		currentPhase = ph;
+	}
+	
 }
-
