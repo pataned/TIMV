005268d36d876763907c7e3286b629369b840afe
pataned <hubgit1@seznam.cz>
2017-04-20 21:26:36 +0200

    Update Chat.java

--- me/pataned/TIMV/InGameOutput/Chat.java
+++ me/pataned/TIMV/InGameOutput/Chat.java
@@ -1,6 +1,11 @@
 package me.pataned.TIMV.InGameOutput;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
 public class Chat {
+	
 	
 	/*
 	 *====== ZÁKONY FARIEB =======
@@ -14,4 +19,37 @@ public class Chat {
 	 * 
 	 */
 
+	public static void broadcast(String prefix, String text){
+		Bukkit.broadcastMessage(prefix + text);
+	}
+	
+	public static void broadcast(String prefix, String text, Player array){
+		array.sendMessage(prefix + text);
+	}
+	
+	public static Player getArrayPlayer(boolean auto){
+		if (auto){
+			for (Player p : Bukkit.getOnlinePlayers()){
+				return p;
+			}
+		}
+		return null;
+	}
+	
+	public static void send(Player p, String prefix, String text){
+		p.sendMessage(prefix + text);
+	}
+	
+	public static String newPrefix(boolean brackets, ChatColor bracketclr, boolean hranate, String body, ChatColor bodyclr){
+		if (brackets){
+			if (hranate){
+				return bracketclr + "[" + bodyclr + body + bracketclr + "] ";
+			} else {
+				return bracketclr + "(" + bodyclr + body + bracketclr + ") ";
+			}
+		} else {
+			return bodyclr + body + bracketclr + " > " + ChatColor.GRAY;
+		}
+	}
+	
 }
