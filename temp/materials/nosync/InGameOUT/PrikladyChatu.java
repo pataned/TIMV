23f48f950f1806ece82a1baffd37d07b37a5095a
vlcik128 <vlckoadam1@gmail.com>
2017-04-20 21:03:27 +0200

    Update and rename temp/materials/nosync/TODO.java to temp/materials/nosync/InGameOUT/PrikladyChatu.java

---
+++ temp/materials/nosync/InGameOUT/PrikladyChatu.java
@@ -0,0 +1,5 @@
+// PRIKLADY: 
+
+String s = ChatColor.GRAY + "Hrac " + ChatColor.RED + p.getName() + " zabil hraca " + ChatColor.GRAY + p.getName();
+String s2 = ChatColor.GRAY + "Mas nastaveny gamemode " + ChatColor.YELLOW + p.getGameMode();
+String s3 = ChatColor.GRAY + "Hra startuje za " + ChatColor.GREEN + i + ChatColor.GRAY + " sekund."
