11ba01124418802a8603885cbef1114c07e56ca8
pataned <hubgit1@seznam.cz>
2017-04-20 18:43:35 +0200

    Update Main.java

--- me/pataned/TIMV/Main.java
+++ me/pataned/TIMV/Main.java
@@ -1,14 +1,24 @@
+
 package me.pataned.TIMV;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me.pataned.TIMV.Threads.CountDown.ZacatecniOdpocitavani;
+
 public class Main extends JavaPlugin{
-	
+
+	private ZacatecniOdpocitavani o1 = new ZacatecniOdpocitavani();
+
 	public static Main plugin;
 	public void onEnable(){
 		plugin = this;
+		o1.start();
 	}
 	
-	
+	public void onDisable(){
+		o1.stop();
+	}
+
+
 
 }
