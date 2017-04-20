3d1a8df7b085a1bfdf11ea2a5d4204c997a13ec4
pataned <hubgit1@seznam.cz>
2017-04-20 18:44:45 +0200

    Create ZacatecniOdpocitavani.java

---
+++ me/pataned/TIMV/Threads/CountDown/ZacatecniOdpocitavani.java
@@ -0,0 +1,31 @@
+package me.pataned.TIMV.Threads.CountDown;
+
+import org.bukkit.Bukkit;
+
+import me.pataned.TIMV.Main;
+import me.pataned.TIMV.Threads.Odpocitavani;
+
+public class ZacatecniOdpocitavani extends Odpocitavani{
+	
+	int id;
+	
+	int i;
+
+	public void start() {
+		id = Bukkit.getScheduler().scheduleSyncRepeatingTask(Main.plugin, new Runnable(){
+			public void run(){
+				i++;
+				
+			}
+		}, 0L, 20L);
+		
+	}
+
+	public void stop() {
+		Bukkit.getScheduler().cancelTask(id);
+		
+	}
+	
+	
+
+}
