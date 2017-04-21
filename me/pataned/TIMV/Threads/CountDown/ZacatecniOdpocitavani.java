bc6572496c699898d17cd7da6226746961d7a2ea
vlcik128 <vlckoadam1@gmail.com>
2017-04-21 17:13:51 +0200

    Update ZacatecniOdpocitavani.java
    Added (temp): Chat.broadcastExceptConsole(Chat.newTempPrefix(...),...);

--- me/pataned/TIMV/Threads/CountDown/ZacatecniOdpocitavani.java
+++ me/pataned/TIMV/Threads/CountDown/ZacatecniOdpocitavani.java
@@ -1,8 +1,10 @@
 package me.pataned.TIMV.Threads.CountDown;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 
 import me.pataned.TIMV.Main;
+import me.pataned.TIMV.InGameOutput.Chat;
 import me.pataned.TIMV.Threads.Odpocitavani;
 
 public class ZacatecniOdpocitavani extends Odpocitavani{
@@ -10,12 +12,13 @@ public class ZacatecniOdpocitavani extends Odpocitavani{
 	int id;
 	
 	int i;
-
+	// TODO Po prenose do pocitaca - riadok: Chat.broadcastExceptConsole(Chat.newTempPrefix(ChatColor.GRAY, ChatColor.DARK_RED, ChatColor.YELLOW, "PrefixText"), "Testing the prefix - " + ChatColor.GREEN + i);
+	//                                       Vymazat
 	public void start() {
 		id = Bukkit.getScheduler().scheduleSyncRepeatingTask(Main.plugin, new Runnable(){
 			public void run(){
 				i++;
-				
+				Chat.broadcastExceptConsole(Chat.newTempPrefix(ChatColor.GRAY, ChatColor.DARK_RED, ChatColor.YELLOW, "PrefixText"), "Testing the prefix - " + ChatColor.GREEN + i);
 			}
 		}, 0L, 20L);
 		
