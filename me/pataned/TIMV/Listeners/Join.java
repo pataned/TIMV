e3e765360c3dec5e52599c9ca27e27ad0e165afc
pataned <hubgit1@seznam.cz>
2017-04-21 19:44:59 +0200

    Update Join.java

--- me/pataned/TIMV/Listeners/Join.java
+++ me/pataned/TIMV/Listeners/Join.java
@@ -1,5 +1,22 @@
 package me.pataned.TIMV.Listeners;
 
-public class Join {
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import me.pataned.TIMV.InGameOutput.Chat;
+
+public class Join implements Listener{
+	
+	@EventHandler
+	public void onJoin(PlayerJoinEvent e){
+		Player p = e.getPlayer();
+		e.setJoinMessage(Chat.newTempPrefix(ChatColor.DARK_GREEN, ChatColor.DARK_RED, ChatColor.GRAY, "TIMV-Join") + "Hrac " + ChatColor.RED + p.getName() + ChatColor.GRAY + " se pripojil.");
+		
+	}
+	
+	
 
 }
