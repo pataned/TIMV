6babb35030d9bfa074b55d0ef3a72687e7bfa231
pataned <hubgit1@seznam.cz>
2017-04-22 14:05:51 +0200

    Update Quit.java

--- me/pataned/TIMV/Listeners/Quit.java
+++ me/pataned/TIMV/Listeners/Quit.java
@@ -1,9 +1,30 @@
 package me.pataned.TIMV.Listeners;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import me.pataned.TIMV.InGameOutput.Chat;
+import me.pataned.TIMV.Threads.CountDown.ZacatecniOdpocitavani;
 
 public class Quit implements Listener{
 	
+	@EventHandler
+	public void onQuit(PlayerQuitEvent e){
+		Player p = e.getPlayer();
+		e.setQuitMessage(Chat.newTempPrefix(ChatColor.DARK_GREEN, ChatColor.DARK_RED, ChatColor.GRAY, "TIMV-Leave") + "Hrac " + ChatColor.RED + p.getName() + ChatColor.GRAY + " se odpojil.");
+		if (Bukkit.getOnlinePlayers().size() < 2){
+			ZacatecniOdpocitavani o = new ZacatecniOdpocitavani();
+			Chat.broadcastWithConsole(Chat.newTempPrefix(ChatColor.DARK_GREEN, ChatColor.DARK_RED, ChatColor.GRAY, "Odpocitavani"), "Na serveru je nedostatek hracu pro start hry!");
+			o.stop();
+			
+			
+		}
+		
+	}
 	
 
 }
