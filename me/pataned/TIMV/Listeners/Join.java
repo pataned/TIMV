a67e5e1b2f4266e6545d731b99b112f820afe69e
pataned <hubgit1@seznam.cz>
2017-04-22 11:52:25 +0200

    Update Join.java

--- me/pataned/TIMV/Listeners/Join.java
+++ me/pataned/TIMV/Listeners/Join.java
@@ -1,5 +1,6 @@
 package me.pataned.TIMV.Listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -7,13 +8,25 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import me.pataned.TIMV.InGameOutput.Chat;
+import me.pataned.TIMV.Threads.CountDown.ZacatecniOdpocitavani;
 
 public class Join implements Listener{
+	
+	private int PocetHracov;
+	
+	
 	
 	@EventHandler
 	public void onJoin(PlayerJoinEvent e){
 		Player p = e.getPlayer();
 		e.setJoinMessage(Chat.newTempPrefix(ChatColor.DARK_GREEN, ChatColor.DARK_RED, ChatColor.GRAY, "TIMV-Join") + "Hrac " + ChatColor.RED + p.getName() + ChatColor.GRAY + " se pripojil.");
+		if (Bukkit.getOnlinePlayers().size() >= 2){
+			ZacatecniOdpocitavani o = new ZacatecniOdpocitavani();
+			Chat.broadcastWithConsole(Chat.newTempPrefix(ChatColor.DARK_GREEN, ChatColor.DARK_RED, ChatColor.GRAY, "Odpocitavani"), "Na serveru je dostatek hracu pro start hry!");
+			o.start();
+			
+			
+		}
 		
 	}
 	
