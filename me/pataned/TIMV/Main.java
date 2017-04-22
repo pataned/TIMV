b1cfb327043447100e7c6c33a5e3cabe78ae006b
pataned <hubgit1@seznam.cz>
2017-04-22 16:35:55 +0200

    Update Main.java

--- me/pataned/TIMV/Main.java
+++ me/pataned/TIMV/Main.java
@@ -4,6 +4,7 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.pataned.TIMV.Listeners.Join;
+import me.pataned.TIMV.Listeners.Quit;
 import me.pataned.TIMV.enumerators.GamePhase;
 
 public class Main extends JavaPlugin{
@@ -23,6 +24,7 @@ public class Main extends JavaPlugin{
 	}
 	private void registerEvents(PluginManager pm){
 		pm.registerEvents(new Join(), this);
+		pm.registerEvents(new Quit(), this);
 	}
 
 
